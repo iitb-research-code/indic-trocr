182c525547b1b2455b83200b66ac2dbc2ff2f180
Dishant Padalia <64226014+dishant26@users.noreply.github.com>
2023-01-30 15:10:50 +0530

    Changes train.py

--- hindi/pretrained/scripts/train.py
+++ hindi/pretrained/scripts/train.py
@@ -112,8 +112,8 @@ print("Number of validation examples:", len(eval_dataset))
 training_args = Seq2SeqTrainingArguments(
     predict_with_generate=True,
     evaluation_strategy="steps",
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
     output_dir="./",
     logging_steps=2,
     save_steps=200,
