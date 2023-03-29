d47b02e1f1cb435bfcc220c4347547ee44c1aa95
Chinmay <chinmay.cc.06@gmail.com>
2023-03-29 14:15:12 +0530

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -12,15 +12,18 @@ feature_extractor=ViTFeatureExtractor.from_pretrained(encode)
 tokenizer = RobertaTokenizer.from_pretrained(decode)
 processor = TrOCRProcessor(feature_extractor=feature_extractor, tokenizer=tokenizer)
 
-model = VisionEncoderDecoderModel.from_pretrained("checkpoint-20600")
+model = VisionEncoderDecoderModel.from_pretrained("/home/pageocr/trocr/hindi/checkpoints/checkpoint-54000")
 
 def preview(image_path):
     image = Image.open(image_path).convert("RGB")
     pixel_values = processor(image, return_tensors="pt").pixel_values
     generated_ids = model.generate(pixel_values)
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    plt.imshow(image)
-    print(generated_text)
+    # plt.imshow(image)
+    print(generated_text, ",", img)
     
-image_path = "/home/venkat/trocr_hindi/dataset/HindiSeg/test/11/1/1.jpg"
-preview(image_path=image_path)
+with open("/data/BADRI/IHTR/testset_small/devanagari/test.txt") as f:
+    for line in f:
+        img = line[5:]
+        image_path = "/data/BADRI/IHTR/testset_small/devanagari/images/{}".format(img)
+        preview(image_path=image_path)
