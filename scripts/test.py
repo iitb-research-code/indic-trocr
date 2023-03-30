0dd7a33d24f487e782dd4531a133c986c17e0905
Chinmay <chinmay.cc.06@gmail.com>
2023-03-30 18:31:55 +0530

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -6,13 +6,13 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 encode = 'google/vit-base-patch16-224-in21k'
-decode = 'flax-community/roberta-hindi'
+decode = 'RahulRaman/Telugu-LM-RoBERTa'
 
 feature_extractor=ViTFeatureExtractor.from_pretrained(encode)
 tokenizer = RobertaTokenizer.from_pretrained(decode)
 processor = TrOCRProcessor(feature_extractor=feature_extractor, tokenizer=tokenizer)
 
-model = VisionEncoderDecoderModel.from_pretrained("checkpoint-20600")
+model = VisionEncoderDecoderModel.from_pretrained("")
 
 def preview(image_path):
     image = Image.open(image_path).convert("RGB")
@@ -22,5 +22,5 @@ def preview(image_path):
     plt.imshow(image)
     print(generated_text)
     
-image_path = "/home/venkat/trocr_hindi/dataset/HindiSeg/test/11/1/1.jpg"
+image_path = "/home/venkat/trocr_hindi/telugu/dataset/test/11/1/1.jpg"
 preview(image_path=image_path)
