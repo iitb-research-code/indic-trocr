fc1ddc78c5410f5ea0f53962799cf151ed0d969a
Chinmay <chinmay.cc.06@gmail.com>
2023-03-30 11:22:56 +0530

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -6,21 +6,27 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 encode = 'google/vit-base-patch16-224-in21k'
-decode = 'flax-community/roberta-hindi'
+decode = 'RahulRaman/Malayalam-LM-RoBERTa'
 
 feature_extractor=ViTFeatureExtractor.from_pretrained(encode)
 tokenizer = RobertaTokenizer.from_pretrained(decode)
 processor = TrOCRProcessor(feature_extractor=feature_extractor, tokenizer=tokenizer)
 
-model = VisionEncoderDecoderModel.from_pretrained("checkpoint-20600")
+model = VisionEncoderDecoderModel.from_pretrained("/home/pageocr/trocr/malayalam/checkpoints/checkpoint-14000")
 
 def preview(image_path):
     image = Image.open(image_path).convert("RGB")
     pixel_values = processor(image, return_tensors="pt").pixel_values
     generated_ids = model.generate(pixel_values)
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    plt.imshow(image)
-    print(generated_text)
-    
-image_path = "/home/venkat/trocr_hindi/dataset/HindiSeg/test/11/1/1.jpg"
-preview(image_path=image_path)
+    # plt.imshow(image)
+    file = open("/home/pageocr/trocr/malayalam/results/result.txt", "a")
+    text = generated_text + ',' + img
+    file.write(text)
+    print(generated_text, ",", img)
+
+with open("/data/BADRI/IHTR/testset_small/malayalam/text.txt") as f:
+    for line in f:
+        img = line[5:]
+        image_path = "/data/BADRI/IHTR/testset_small/malayalam/images/{}".format(img.strip('\n'))
+        preview(image_path=image_path)
