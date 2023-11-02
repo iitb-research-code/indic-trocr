a1866dd499f7084a6fa5fd693cefdba402778d8e
Ishan Darshan Biyani <92304955+Biyani404198@users.noreply.github.com>
2023-11-02 02:58:18 -0700

    add config.py

---
+++ scripts/config.py
@@ -0,0 +1,16 @@
+ENCODER = 'google/vit-base-patch16-224-in21k'
+DECODERS = {
+    "Gujarati": "l3cube-pune/gujjarati-bert",
+    "Bengali": "l3cube-pune/bengali-bert",
+    "Gurumukhi": "flax-community/roberta-hindi",
+    "Hindi": "flax-community/roberta-hindi",
+    "Kannada": "Chakita/KannadaBERT",
+    "Malayalam": "RahulRaman/Malayalam-LM-RoBERTa",
+    "Odia": "flax-community/roberta-hindi",
+    "Tamil": "d42kw01f/Tamil-RoBERTa",
+    "Telugu": "RahulRaman/Telugu-LM-RoBERTa",
+    "Urdu": "urduhack/roberta-urdu-small"
+}
+
+lang_set1 = ['Gujarati', 'Gurumukhi', 'Hindi', 'Odia', 'Telugu', 'Urdu']
+lang_set2 = ['Bengali', 'Kannada', 'Malayalam', 'Tamil']
